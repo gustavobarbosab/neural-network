dddfe56a4a16150e52fcaa5c8b7973575577b688
Gustavo Barbosa <gustavobarbosabarreto@gmail.com>
2023-01-31 09:31:13 -0300

    ajustando leitura do modelo salvo

--- cnn-emnist.jl
+++ cnn-emnist.jl
@@ -2,131 +2,137 @@ ENV["JULIA_CUDA_SILENT"] = true
 using LinearAlgebra, Statistics, Flux, MLDatasets, Plots
 using BetaML: ConfusionMatrix, fit!, info
 using Printf
-using BSON: @save, @load
+using Zygote
+using BSON
 
 function load_train_dataset()
-	x_treino, y_treino = MLDatasets.EMNIST(:letters, Tx = Float32, split = :train)[:] # Pega o conjunto de treinamento do dataset
-	x_treino = permutedims(x_treino, (2, 1, 3)) # troca os eixos weight e height de lugar
-	x_treino = reshape(x_treino, (28, 28, 1, 124800)) # transforma a matriz criado em 124800 matrizes 28x28 com 1 dimensao
-	y_treino = Flux.onehotbatch(y_treino, 0:36) # codifica os campos em uma matriz de 1's e 0's usando 37 possiveis classes de valores.
-	dados_treino = Flux.Data.DataLoader((x_treino, y_treino), batchsize = 128) # Us'=ado para carregar os dados em lotes
-	return dados_treino
+    x_treino, y_treino = MLDatasets.EMNIST(:letters, Tx=Float32, split=:train)[:] # Pega o conjunto de treinamento do dataset
+    x_treino = permutedims(x_treino, (2, 1, 3)) # troca os eixos weight e height de lugar
+    x_treino = reshape(x_treino, (28, 28, 1, 124800)) # transforma a matriz criado em 124800 matrizes 28x28 com 1 dimensao
+    y_treino = Flux.onehotbatch(y_treino, 0:36) # codifica os campos em uma matriz de 1's e 0's usando 37 possiveis classes de valores.
+    dados_treino = Flux.Data.DataLoader((x_treino, y_treino), batchsize=128) # Us'=ado para carregar os dados em lotes
+    return dados_treino
 end
 
-function load_test_dataset() 
-    x_teste, y_teste = MLDatasets.EMNIST(:letters, Tx = Float32, split = :test)[:] # Pega o conjunto de teste do dataset
-	x_teste = permutedims(x_teste, (2, 1, 3)) # Permuta os eixos weight e height de lugar
-	x_teste = reshape(x_teste, (28, 28, 1, 20800)) # Transforma os arrays criado em 20800 matrizes de 28x28
-	y_teste = Flux.onehotbatch(y_teste, 0:36) # codifica os campos em uma matriz de 1's e 0's usando 37 possiveis classes de valores.
+function load_test_dataset()
+    x_teste, y_teste = MLDatasets.EMNIST(:letters, Tx=Float32, split=:test)[:] # Pega o conjunto de teste do dataset
+    x_teste = permutedims(x_teste, (2, 1, 3)) # Permuta os eixos weight e height de lugar
+    x_teste = reshape(x_teste, (28, 28, 1, 20800)) # Transforma os arrays criado em 20800 matrizes de 28x28
+    y_teste = Flux.onehotbatch(y_teste, 0:36) # codifica os campos em uma matriz de 1's e 0's usando 37 possiveis classes de valores.
     return x_teste, y_teste
 end
 
 function create_model()
-	return Chain(
-		Conv((3, 3), 1 => 16, pad = (1, 1), relu), # recebera uma imagem 28x28 e fara uma convolucao com 8 filtros 5x5, e depois uma linearizacao usando relu. Com isso temos: (28 + 2*2 - 5)/2 + 1 = 14,5 => floor(14,5) = 14
-		MaxPool((2, 2)),
-		Conv((3, 3), 16 => 32, pad = (1, 1), relu),
-		MaxPool((2, 2)),
-		Conv((3, 3), 32 => 32, pad = (1, 1), relu),
-		MaxPool((2, 2)),
-		Flux.flatten, # faz uma linearizacao
-		Dense(288, 37), # Cria uma rede totalmente conectada de entrada 32 e saida 10
-		Flux.softmax, # aplica sotfmax para dar a probabilidade de ser cada uma das 37 classes
-	)
+    return Chain(
+        Conv((3, 3), 1 => 16, pad=(1, 1), relu), # recebera uma imagem 28x28 e fara uma convolucao com 8 filtros 5x5, e depois uma linearizacao usando relu. Com isso temos: (28 + 2*2 - 5)/2 + 1 = 14,5 => floor(14,5) = 14
+        MaxPool((2, 2)),
+        Conv((3, 3), 16 => 32, pad=(1, 1), relu),
+        MaxPool((2, 2)),
+        Conv((3, 3), 32 => 32, pad=(1, 1), relu),
+        MaxPool((2, 2)),
+        Flux.flatten, # faz uma linearizacao
+        Dense(288, 37), # Cria uma rede totalmente conectada de entrada 32 e saida 10
+        Flux.softmax, # aplica sotfmax para dar a probabilidade de ser cada uma das 37 classes
+    )
 end
 
-modelo = create_model();
+function print_confusion_matrix(x_treino, x_teste, y_treino, y_teste)
+    ŷtreino = modelo(x_treino)
+    ŷteste = modelo(x_teste)
+
+    acuracia(ŷtreino, y_treino)
+    acuracia(ŷteste, y_teste)
+
+    Flux.onecold(y_treino[:, 2]) - 1  # rótulo da amostra 2
+    plot(Gray.(x_treino[:, :, 1, 2]))   # imagem da amostra 2
+
+    cm = ConfusionMatrix()
+    fit!(cm, Flux.onecold(y_teste) .- 1, Flux.onecold(ŷteste) .- 1)
+    print(cm)
+
+    res = info(cm)
+
+    heatmap(string.(res["categories"]),
+        string.(res["categories"]),
+        res["normalised_scores"],
+        seriescolor=cgrad([:white, :blue]),
+        xlabel="Predito",
+        ylabel="Real",
+        title="Matriz de Confusão (scores normalizados)")
+
+    # Limita o mapa de cores, para vermos melhor onde os erros estão
+
+    heatmap(string.(res["categories"]),
+        string.(res["categories"]),
+        res["normalised_scores"],
+        seriescolor=cgrad([:white, :blue]),
+        clim=(0.0, 0.02),
+        xlabel="Predito",
+        ylabel="Real",
+        title="Matriz de Confusão (scores normalizados)")
+end
 
 acuracia(ŷ, y) = (mean(Flux.onecold(ŷ) .== Flux.onecold(y)))
-perda(x, y) = Flux.crossentropy(modelo(x), y)
 
-opt = Flux.ADAM()
-ps = Flux.params(modelo)
+function train()
+    modelo = create_model()
 
-num_épocas = 5
+    perda(x, y) = Flux.crossentropy(modelo(x), y)
 
-dados_treino = load_train_dataset()
-x_teste, y_teste = load_test_dataset()
+    opt = Flux.ADAM()
+    ps = Flux.params(modelo)
 
-@info("Starting the training...")
-best_acc = 0.0
-last_improvement = 0
-for época in 1:num_épocas
-	println("Época ", época)
-	Flux.train!(perda, ps, dados_treino, opt)
+    num_épocas = 8
 
-	# Calcule a acurácia:
-	ŷteste = modelo(x_teste)
-	acu = acuracia(ŷteste, y_teste)
+    dados_treino = load_train_dataset()
+    x_teste, y_teste = load_test_dataset()
 
-	@info(@sprintf("[%d]: Acurácia nos testes: %.4f", época, acu))
-	# Se a acurácia for muito boa, termine o treino
-	if acu >= 0.999
-		@info(" -> Término prematuro: alcançamos uma acurácia de 99.9%")
-		break
-	end
+    @info("Starting the training...")
+    best_acc = 0.0
+    last_improvement = 0
+    for época in 1:num_épocas
+        println("Época ", época)
+        Flux.train!(perda, ps, dados_treino, opt)
 
-	if acu >= best_acc
-		@info("We have a new best accuracy! Saving it...")
-		@save "emnist_conv.bson" ps
-		global best_acc = acu
-		global last_improvement = época
-	end
+        # Calcule a acurácia:
+        ŷteste = modelo(x_teste)
+        acu = acuracia(ŷteste, y_teste)
 
+        @info(@sprintf("[%d]: Acurácia nos testes: %.4f", época, acu))
+        # Se a acurácia for muito boa, termine o treino
+        if acu >= 0.999
+            @info(" -> Término prematuro: alcançamos uma acurácia de 99.9%")
+            break
+        end
+
+        if acu >= best_acc
+            @info("We have a new best accuracy! Saving it...")
+            BSON.@save "emnist_conv.bson" modelo
+            best_acc = acu
+            last_improvement = época
+        end
+
+    end
 end
 
-ŷtreino = modelo(x_treino)
-ŷteste = modelo(x_teste)
-
-acuracia(ŷtreino, y_treino)
-acuracia(ŷteste, y_teste)
-
-Flux.onecold(y_treino[:, 2]) - 1  # rótulo da amostra 2
-plot(Gray.(x_treino[:, :, 1, 2]))   # imagem da amostra 2
-
-cm = ConfusionMatrix()
-fit!(cm, Flux.onecold(y_teste) .- 1, Flux.onecold(ŷteste) .- 1)
-print(cm)
-
-res = info(cm)
-
-heatmap(string.(res["categories"]),
-	string.(res["categories"]),
-	res["normalised_scores"],
-	seriescolor = cgrad([:white, :blue]),
-	xlabel = "Predito",
-	ylabel = "Real",
-	title = "Matriz de Confusão (scores normalizados)")
-
-# Limita o mapa de cores, para vermos melhor onde os erros estão
-
-heatmap(string.(res["categories"]),
-	string.(res["categories"]),
-	res["normalised_scores"],
-	seriescolor = cgrad([:white, :blue]),
-	clim = (0.0, 0.02),
-	xlabel = "Predito",
-	ylabel = "Real",
-	title = "Matriz de Confusão (scores normalizados)")
-
-
-
 # Testing the model, from saved model
-function test(; kws...)    
-    # Loading the test data
-    x_teste, y_teste = load_test_dataset()
-    
-    # Re-constructing the model with random initial weights
-    model = create_model()
-    
-    # Loading the saved parameters
-    BSON.@load "emnist_conv.bson" params
-    
-    # Loading parameters onto the model
-    Flux.loadparams!(model, params)
+function test()
 
-    model_test = model(x_teste)
-	acu = acuracia(model_test, y_teste)
+    @info "Loading the test data"
+    x_teste, y_teste = load_test_dataset()
+
+    @info "Re-constructing the model with random initial weights"
+    newModel = create_model()
+
+    @info "Loading saved model"
+    BSON.@load "emnist_conv.bson" modelo
+
+    @info "Loading parameters onto the model"
+    Flux.loadmodel!(newModel, modelo)
+
+    model_test = newModel(x_teste)
+    @info "Getting the accuracy"
+    acu = acuracia(model_test, y_teste)
 
     @show acu
 end
